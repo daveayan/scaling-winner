cc6e0328df85874c9935fb5536d13a8170abbfc5
Ayan Dave <daveayan@gmail.com>
2022-05-08 07:14:34 -0400

    Translation Service

---
+++ src/main/java/com/daveayan/scalingwinner/greeting/translator/GreetingTranslator.java
@@ -0,0 +1,61 @@
+package com.daveayan.scalingwinner.greeting.translator;
+
+public class GreetingTranslator {
+    String fromLanguage;
+    String toLanguage;
+    String incomingText;
+    String translatedText;
+    String status;
+
+    public GreetingTranslator() {
+    }
+
+    public GreetingTranslator(String fromLanguage, String toLanguage, String incomingText, String translatedText, String status) {
+        this.fromLanguage = fromLanguage;
+        this.toLanguage = toLanguage;
+        this.incomingText = incomingText;
+        this.translatedText = translatedText;
+        this.status = status;
+    }
+
+    public String getFromLanguage() {
+        return this.fromLanguage;
+    }
+
+    public void setFromLanguage(String fromLanguage) {
+        this.fromLanguage = fromLanguage;
+    }
+
+    public String getToLanguage() {
+        return this.toLanguage;
+    }
+
+    public void setToLanguage(String toLanguage) {
+        this.toLanguage = toLanguage;
+    }
+
+    public String getIncomingText() {
+        return this.incomingText;
+    }
+
+    public void setIncomingText(String incomingText) {
+        this.incomingText = incomingText;
+    }
+
+    public String getTranslatedText() {
+        return this.translatedText;
+    }
+
+    public void setTranslatedText(String translatedText) {
+        this.translatedText = translatedText;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
---
+++ src/main/java/com/daveayan/scalingwinner/greeting/translator/TranslatorController.java
@@ -0,0 +1,44 @@
+package com.daveayan.scalingwinner.greeting.translator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/translate")
+public class TranslatorController {
+    private static final Logger LOG = LoggerFactory.getLogger(TranslatorController.class);
+
+    private Map<String, String> translations = new HashMap<String, String>();
+
+    public TranslatorController() {
+        translations.put("Hello", "Hola");
+        translations.put("Thank You", "Gracias");
+        translations.put("Bye", "Adios");
+    }
+
+    @GetMapping("")
+    ResponseEntity<GreetingTranslator> getTranslatedString(
+        @RequestParam String text,
+        @RequestParam String from,
+        @RequestParam String to
+    ) {
+        LOG.trace("IN getTranslatedString");
+        String translatedText = text;
+        String status = "Not Found";
+        if(translations.containsKey(text)) {
+            translatedText = translations.get(text);
+            status = "Found";
+        }
+        GreetingTranslator t = new GreetingTranslator(text, translatedText, from, to, status);
+        return new ResponseEntity<GreetingTranslator>(t, HttpStatus.OK);
+    }
+}
